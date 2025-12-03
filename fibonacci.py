e6d839c160a216921e36b56da342b8deef0d316d
Jeferson da Silveira Kendzierski <jefo.silveira@gmail.com>
2025-12-03 00:42:30 -0300

    Aprimoramento tratamento de erros
    Realizada aprimoração do tratamento de erros, corrigindo o except e adicionando o while para retornar a digitação caso os dados digitados sejam inválidos.

--- fibonacci.py
+++ fibonacci.py
@@ -1,21 +1,24 @@
 anterior = 0
 proximo = 1
+flag = True
 
-try:
-    quantidade = int(input())
+while flag:
+    try:
+        quantidade = int(input())
 
-    if quantidade <=0:
-        raise ValueError("Erro 1")
-    
-except Exception (ValueError, TypeError):
-    print("ERRO: Dados digitados são inválidos!")
+        if quantidade <=0:
+            raise ValueError("Erro 1")
+        else:
+            flag = False
+        
+    except (ValueError, TypeError):
+        print("ERRO: Dados digitados são inválidos!")
 
 else:
 
     print(anterior, proximo, end=' ')
 
     for i in range(0, (quantidade -2)):
-
         atual = anterior + proximo
         print(atual, end=' ')
         anterior = proximo
