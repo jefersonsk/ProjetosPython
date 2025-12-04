61bde5389df60da6af4fd809eb855ed01fa38541
jeffnuvas <jefo.silveira@gmail.com>
2025-12-04 14:54:45 -0300

    Não concluído
    Falta parte principal do programa, verificar o esporte digitado e informar a porcentagem.

---
+++ LIsta_Exercícios_Arquivos_Q05.py
@@ -0,0 +1,14 @@
+lista_nomes = dados_esportes = []
+
+esporte = input("Digite um nome de esporte: ")
+
+with open("pessoas.csv", "r") as arquivo:
+    arquivo.readline()
+    for linha in arquivo:
+        dados = linha.split(",")
+        dados_esportes.append([dados[0], dados[7].strip()])
+       
+        # if dados_esportes[1].upper()== esporte:
+        #    lista_nomes.append[dados[0]]
+
+print(dados_esportes[1])
