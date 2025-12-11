7fcefb1114894fb3184a2a21a6f5cef285e0c93d
jeffnuvas <jefo.silveira@gmail.com>
2025-12-11 11:45:15 -0300

    Exercício concluído.

---
+++ Beecrowd/bee_1038.py
@@ -0,0 +1,9 @@
+lanches = {1: 4.00, 2: 4.50, 3: 5.00, 4: 2.00, 5: 1.50}
+
+escolha, quantidade = map(int, input().split(" "))
+
+valor = lanches[escolha]
+
+total = valor * quantidade
+
+print(f"Total: R$ {total:.2f}")
