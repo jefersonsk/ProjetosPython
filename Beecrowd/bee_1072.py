f3480298a0f7e0fd3e86de93ded1ca7cfef8086a
jeffnuvas <jefo.silveira@gmail.com>
2025-12-17 07:21:04 -0300

    Exercício concluído.

--- Beecrowd/bee_1072.py
+++ Beecrowd/bee_1072.py
@@ -1,4 +1,14 @@
+conta_in = 0
+conta_out = 0
+
 numero = int(input())
 
 for x in range(numero):
     verifica = int(input())
+    if verifica >= 10 and verifica <= 20:
+        conta_in += 1
+    else:
+        conta_out += 1
+
+print(f"{conta_in} in")
+print(f"{conta_out} out")
