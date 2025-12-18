045386ce109660d944aa3ac746bd82f1cae2cdb1
jeffnuvas <jefo.silveira@gmail.com>
2025-12-18 11:25:14 -0300

    Exercício concluído

---
+++ Curso em Vídeo/Mundo 2/exercicio_053.py
@@ -0,0 +1,11 @@
+frase = input()
+
+frase = frase.replace(" ", "")
+invertida = frase[::-1]
+
+print(frase, invertida)
+
+if frase == invertida:
+    print("A frase é um palíndromo")
+else:
+    print("A frase não é um palíndromo")
