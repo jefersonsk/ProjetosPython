8420eb394efbe820491223b8b19b37c0220d709d
jeffnuvas <jefo.silveira@gmail.com>
2025-12-18 12:03:37 -0300

    Exercício concluído.

---
+++ Beecrowd/bee_1118.py
@@ -0,0 +1,27 @@
+flag = True
+contador = 0
+soma = 0
+
+while flag:
+    valida = True
+    nota = float(input())
+
+    if not 0 <= nota <= 10:
+        print("nota invalida")
+    else:
+        soma += nota
+        contador += 1
+
+    if contador == 2:
+        print(f"media = {soma / contador:.2f}")
+
+        while valida:
+            continua = int(input("novo calculo (1-sim 2-nao)\n"))
+
+            if continua == 1:
+                soma = 0
+                contador = 0
+                valida = False
+            elif continua == 2:
+                valida = False
+                flag = False
