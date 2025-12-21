c90aa7b7459832383cf68723f0a96ad71ab2145c
Jeferson da Silveira Kendzierski <jefo.silveira@gmail.com>
2025-12-21 00:11:04 -0300

    Exercício concluído.

--- Beecrowd/bee_1146.py
+++ Beecrowd/bee_1146.py
@@ -1,13 +1,2 @@
-flag = True
-
-while flag:
-    numero = int(input())
-    if numero ==0:
-        flag = False
-    else:
-        for x in range(1, numero + 1):
-            if x != numero:
-                print(f"{x}", end=" ")
-            else:
-                print(f"{x}")
-    numero = 0
+while (numero := int(input())) != 0:
+    print(*range(1, numero + 1), sep=" ")
