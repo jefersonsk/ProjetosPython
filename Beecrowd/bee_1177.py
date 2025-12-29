2b093d798baa9b4f340cf3e5986d228daf9fd0c6
Jeferson da Silveira Kendzierski <jefo.silveira@gmail.com>
2025-12-29 04:05:26 -0300

    Adicionado um comentário.

--- Beecrowd/bee_1177.py
+++ Beecrowd/bee_1177.py
@@ -3,7 +3,7 @@ lista = []
 numero = int(input())
 
 for x in range(1000):
-    lista.append(x % numero)
+    lista.append(x % numero) # O operador faz o trabalho de zerar o contador automaticamente sempre que atingimos o valor de numero
 
 for posicao, numero in enumerate(lista):
     print(f"N[{posicao}] = {numero}")
